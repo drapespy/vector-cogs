dfb0366c3dda7ffb9a07145f7ea4f3bdf1450ed4
Vaughn Fox-Sellers <>
2021-06-07 17:43:56 +0000

    update

--- bot.py
+++ bot.py
@@ -1,4 +1,23 @@
 import discord
 from discord.ext import commands
+from os import getenv
+import aiohttp
+import warnings
 
-bot = commands.Bot
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+class Vector(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+            
+        self.session = aiohttp.ClientSession()
+
+    async def on_ready(self):
+        print('Bot is ready.')
+
+
+bot = Vector(command_prefix='v!', case_insensitive=True)
+
+if __name__ == '__main__':
+    bot.load_extension('jishaku')
+    bot.run(getenv('token'))
